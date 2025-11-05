f503cca2822576b00b3ce5aaf0742b30a5101efe
mani G <gundlamanikumar@gmail.com>
2025-11-05 11:05:54 -0400

    adding catalouge and nginx script to Roboshop

---
+++ catalogue.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Timestamp=$(date +%F-%H-%M-%S)
+Log_file="/tmp/$0-$Timestamp.log"
+
+Validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$R $2 failed $N"
+        exit 1
+    else 
+        echo -e "$G $2 succesfull $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R you need to run this command as a root user $N"
+else 
+    echo -e "$G you are a root user $N"
+fi
+
+dnf module disable nodejs -y &>> $Log_file
+Validate $? "disabling nodejs"
+
+dnf module enable nodejs:18 -y &>> $Log_file
+Validate $? "enabling nodejs"
+
+dnf install nodejs -y &>> $Log_file
+Validate $? "installing nodejs" 
+
+useradd roboshop &>> $Log_file
+Validate $? "added user"
+
+mkdir /app &>> $Log_file
+Validate $? "created app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $Log_file
+Validate $? "downloading applicatino"
+
+cd /app &>> $Log_file
+
+unzip /tmp/catalogue.zip &>> $Log_file
+Validate $? "unzipping catalouge"
+
+cd /app
+
+npm install &>> $Log_file
+Validate $? "downloading dependices"  
+
+cp C\devops\daws76\repos\shell-scripting\roboshop_shell_scripting\ /etc/systemd/system/catalogue.service &>> $Log_file
+
+
+systemctl daemon-reload &>> $Log_file
+Validate $? "reloading daemon"
+
+systemctl enable catalogue &>> $Log_file
+Validate $? "Enabling catalouge"
+
+systemctl start catalogue &>> $Log_file
+Validate $? "starting catalouge"
+
+dnf install mongodb-org-shell -y &>> $Log_file
+Validate $? "Installing Mongodb client"
+
+mongo --host mongodb.chainverse.online </app/schema/catalogue.js &>> $Log_file
+Validate $? "Loading data in mangodb server"
---
+++ web.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+Id=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+Time_stamp=$(date +%D-%H-%M-%S)
+Log_file="/tmp/$0-$Time_stamp.Log"
+
+Validate(){
+    if [ $1 -ne 0 ]
+    then    
+        echo -e "$R $2 failed $N"
+    else
+        echo -e "$G $2 successful $N"
+    fi
+}
+
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R you shoould be root user to run this command $N"
+else 
+    echo -e "$G you are a root user $N"
+fi
+
+dnf install nginx -y &>> $Log_file
+Validate $? "installing nginx"
+
+systemctl enable nginx &>> $Log_file
+Validate $? "enabining nginx"
+
+
+systemctl start nginx &>> $Log_file
+Validate $? "starting nginx"
+
+
+rm -rf /usr/share/nginx/html/* &>> $Log_file
+Validate $? "deleting default code"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $Log_file
+Validate $? "downloadign code"
+
+cd /usr/share/nginx/html &>> $Log_file
+Validate $? "changing directory"
+
+unzip /tmp/web.zip &>> $Log_file
+Validate $? "unzipping code"
+
+cp web.repo /etc/nginx/default.d/roboshop.conf &>> $Log_file
+Validate $? "copied conf"
+
+systemctl restart nginx &>> $Log_file
+Validate $? "restarting nginx"
