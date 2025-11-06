0bd93b0982460e53d852cc60cd207fda2f363e85
mani G <gundlamanikumar@gmail.com>
2025-11-05 20:33:16 -0400

    updating the script and more scripts

--- web.sh
+++ web.sh
@@ -42,7 +42,7 @@ rm -rf /usr/share/nginx/html/* &>> $Log_file
 Validate $? "deleting default code"
 
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $Log_file
-Validate $? "downloadign code"
+Validate $? "downloading code"
 
 cd /usr/share/nginx/html &>> $Log_file
 Validate $? "changing directory"
@@ -50,7 +50,7 @@ Validate $? "changing directory"
 unzip /tmp/web.zip &>> $Log_file
 Validate $? "unzipping code"
 
-cp web.repo /etc/nginx/default.d/roboshop.conf &>> $Log_file
+cp /home/centos/roboshop_shell/web.repo /etc/nginx/default.d/roboshop.conf &>> $Log_file
 Validate $? "copied conf"
 
 systemctl restart nginx &>> $Log_file
