a7531e48d49c07c1a5a40ac0777e579019d5851e
mani G <gundlamanikumar@gmail.com>
2025-11-06 20:59:58 -0400

    updating the script and more scripts

--- cart.sh
+++ cart.sh
@@ -1,73 +1,87 @@
-#!/bin/bash 
+#!/bin/bash
 
 ID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-Timestamp=$(date +%F-%H-%M-%S)
-Log_file="/tmp/$0-$Timestamp.log"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-Validate(){
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo -e "$R $2 failed $N"
+    then
+        echo -e "$2 ... $R FAILED $N"
         exit 1
-    else 
-        echo -e "$G $2 succesfull $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
-then 
-    echo -e "$R you need to run this command as a root user $N"
-    exit 1
-else 
-    echo -e "$G you are a root user $N"
-fi
-
-dnf module disable nodejs -y &>> $Log_file
-Validate $? "Disabling Nodejs"
-
-dnf module enable nodejs:18 -y &>> $Log_file
-Validate $? "Enabling nodejs"
-
-dnf install nodejs -y &>> $Log_file
-Validate $? "Installing Nodejs"
-
-id roboshop &>> $Log_file
-if [ $? -ne 0 ]
-then 
-    useradd roboshop
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-    echo -e "$Y user already exit skipping $N"
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current NodeJS"
+
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+
+VALIDATE $? "Enabling NodeJS:18"
+
+dnf install nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "Installing NodeJS:18"
+
+id roboshop #if roboshop user does not exist, then it is failure
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
 fi
 
-mkdir -p /app &>> $Log_file
-Validate $? "Creating directory"
+mkdir -p /app
 
-curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>> $Log_file
-Validate $? "Downloading cart app" 
+VALIDATE $? "creating app directory"
 
-cd /app
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip  &>> $LOGFILE
 
-unzip -o /tmp/cart.zip &>> $Log_file
-Validate $? "Unzipping cart"
+VALIDATE $? "Downloading cart application"
 
-npm install &>> $Log_file
-Validate $? "Downloading dependencies"
+cd /app 
 
-cp /home/centos/roboshop_shell/cart.repo /etc/systemd/system/cart.service &>> $Log_file
-Validate $? "copied cart conf"
+unzip -o /tmp/cart.zip  &>> $LOGFILE
 
-systemctl daemon-reload &>> $Log_file
-Validate $? "Reloading daemon"
+VALIDATE $? "unzipping cart"
 
-systemctl enable cart &>> $Log_file
-Validate $? "Enabling cart"
+npm install  &>> $LOGFILE
 
-systemctl start cart &>> $Log_file
-Validate $? "Starting cart"
+VALIDATE $? "Installing dependencies"
+
+# use absolute, because cart.service exists there
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+
+VALIDATE $? "Copying cart service file"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "cart daemon reload"
+
+systemctl enable cart &>> $LOGFILE
+
+VALIDATE $? "Enable cart"
+
+systemctl start cart &>> $LOGFILE
+
+VALIDATE $? "Starting cart"
